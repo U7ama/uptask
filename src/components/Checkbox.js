0d23daf240c79864b5f80f302c5727247d8952ba
U7ama <u7amajutt@gmail.com>
2024-07-26 17:00:36 +0500

    effects

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import CheckArrow from "./assets/CheckArrow";
 
 const CheckboxLabel = ({ reff, handlerId, name, checked, onChange }) => (
   <>
@@ -16,19 +17,7 @@ const CheckboxLabel = ({ reff, handlerId, name, checked, onChange }) => (
           aria-label={`Checkbox for ${name}`}
         />
         <CheckMark checked={checked}>
-          <svg
-            width="19"
-            height="14"
-            viewBox="-2 -2 23 18"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M1 7.6L7.03451 12.9672C7.05497 12.9854 7.08626 12.9837 7.1047 12.9635L18 1"
-              stroke="currentColor"
-              strokeLinecap="round"
-            />
-          </svg>
+          <CheckArrow />
         </CheckMark>
       </CheckboxWrapper>
     </Label>
